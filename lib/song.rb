ab7c3963e63f87a15620f0e3f7f2a9073a6cbb35
Maurice Fiorani <mauricef97@gmail.com>
2020-02-18 20:40:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 require 'pry'
 class Song 
-  attr_accessor :name, :artist
+  attr_accessor :name
+  attr_reader :artist 
   @@all = []
   
   def initialize(name)
@@ -12,16 +13,18 @@ class Song
     @@all
   end
   
-  def artist_name=(name)
-  Artist.find_or_create_by_name(name)
-  
+  def artist_name=(artist)
+   if artist.class == Artist 
+     @artist = artist
+   else
+     @artist = Artist.find_or_create_by_name(name)
+    end  
   end
   
   def self.new_by_filename(name)
    split_name = name.split(" - ")
    song = self.new(split_name[1])
-   
-   #song.artist = split_name[0]
+   song.artist = split_name[0]
    
   end
 
