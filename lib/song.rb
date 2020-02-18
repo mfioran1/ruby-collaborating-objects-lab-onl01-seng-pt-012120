a5fcf55049e66ede2b6270829c8ba4fed5b05b11
Maurice Fiorani <mauricef97@gmail.com>
2020-02-18 20:45:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,8 +24,8 @@ class Song
   def self.new_by_filename(name)
    split_name = name.split(" - ")
    song = self.new(split_name[1])
-   #song.artist = split_name[0]
-   
+   song.artist = split_name[0]
+   song 
   end
 
 end  
