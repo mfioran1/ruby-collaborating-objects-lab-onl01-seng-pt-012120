8375277ddff1d1bb141d5356649a1cd48519d264
Maurice Fiorani <mauricef97@gmail.com>
2020-02-18 20:57:22 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,20 @@
+require 'pry'
+class MP3Importer
+  attr_reader :path 
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+  Dir.entries(self.path).select {|file| file.end_with?(".mp3")}
+  end 
+  
+  def import 
+    self.files.each do |file|
+      Song.new_by_filename(file)
+    end
+  end
+    
+  
+end  
