68e0bbe89edebaefb7bab4839127b12c659226e9
Maurice Fiorani <mauricef97@gmail.com>
2020-02-18 20:10:32 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,7 @@ class MP3Importer
   end
   
   def files
-  Dir.entries(self.path).select {|file| file.end_with?(".mp3")
+  Dir.entries(self.path).select {|file| file.end_with?(".mp3")}
   end 
     
   
